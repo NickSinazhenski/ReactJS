d1c9ea1d53fc9b046374f7950e6ce0d2713248ce
“FirstName <nick.sinazhenski@student.ehu.lt>
2025-04-08 08:24:43 +0300

    hw_02_menu_page

--- hw_prj/src/App.jsx
+++ hw_prj/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from '../src/components/Header.jsx';
+import Footer from './components/Footer.jsx';   
+import MenuSection from './components/MenuSection.jsx';
 function App() {
-  return(
-<div>
-  <ul>
-    <li>Genadiy</li>
-    <li>Petya</li>
-    <li>Baha</li>
-    <li>Vasya</li>
-  </ul>
-</div>
-  )
+  return (
+    <>
+      <Header />
+      <div className="skew"></div>
+      <MenuSection />
+      <Footer />
+    </>
+  );
 }
 
 export default App
