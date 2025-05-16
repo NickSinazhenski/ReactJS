6ad9911314847ecd1afa1438f88bcb10321d0c9b
“FirstName <nick.sinazhenski@student.ehu.lt>
2025-05-16 13:07:36 +0200

    feat: add login section with login logout logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,18 @@ import Footer from "../src/components/footer/Footer.jsx";
 import MenuSection from "../src/components/menusection/MenuSection.jsx";
 import CartProvider from "../src/components/menusection/CartContext.jsx";
 import CurveAfterHeader from "../src/components/header/skewafterheader/CurveAfterHeader.jsx";
-import HomePage from "../src/components/homepage/HomePage.jsx";
-
-  // App component
+import HomePage from "./components/homesection/HomePage.jsx";
+import LogSection from "./components/logsection/LogSection.jsx";
 
 const App = () => {
-
-    return (
-      <CartProvider>
-        <Header />
-        < MenuSection/>
-        <Footer />
-      </CartProvider>
-    );
-  };
+  return (
+    <CartProvider>
+      <Header />
+      <CurveAfterHeader />
+      <LogSection />
+      <Footer />
+    </CartProvider>
+  );
+};
 
 export default App;
